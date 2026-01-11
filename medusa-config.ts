1153f124b895a0d03fd4baead6e05cdbfc474c87
AbdelrahmanSuliman <abdelrahmansuliman123@gmail.com>
2026-01-11 17:05:40 +0200

    assert env variables in medusa config

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,21 +1,28 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+const { loadEnv, defineConfig } = require("@medusajs/framework/utils");
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+// Load environment variables based on the current environment
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS,
+      adminCors: process.env.ADMIN_CORS,
+      authCors: process.env.AUTH_CORS,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    },
+  },
+  admin: {
+    backendUrl:
+      process.env.MEDUSA_ADMIN_BACKEND_URL ||
+      "https://zonked-beam-favour.medusajs.app",
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+  },
   modules: [
     {
       resolve: "./src/modules/appointment",
     },
   ],
-  projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
-    http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    },
-  },
 });
--- src/admin/lib/config.ts
+++ src/admin/lib/config.ts
@@ -1,7 +1,7 @@
 import Medusa from "@medusajs/js-sdk";
 
 export const sdk = new Medusa({
-  baseUrl: process.env.MEDUSA_ADMIN_BACKEND_URL || "http://localhost:9000",
+  baseUrl: "https://zonked-beam-favour.medusajs.app",
   debug: process.env.NODE_ENV === "development",
   auth: {
     type: "session",
